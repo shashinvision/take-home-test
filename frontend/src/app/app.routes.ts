192052ef2505c2f1148342defab77c5694bbafc8
Felipe Mancilla <felipei.mancilla@gmail.com>
2026-01-15 11:31:40 -0300

    add app-root with router

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from "@angular/router";
 import { HomeComponent } from "./_pages/home/home.component";
+import { LoginComponent } from "./_pages/login/login.component";
 
 export const routes: Routes = [
   {
-    path: "",
+    path: "home",
     component: HomeComponent,
+  },
+  {
+    path: "login",
+    component: LoginComponent,
   },
 ];
